5d532c372ed4a2a79959379cdf4014e6ea830cff
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-14 15:09:05 +0500

    getServerSideProps basic implementation done

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -1,20 +1,29 @@
 import Layout from '@components/Layout'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { IEvent } from '@interfaces/event.interface'
+import React from 'react'
+import axios from 'axios'
+import { API_URL } from '@config/index'
 
-const EvenItem = () => {
+const EvenItem: React.FC<IEventItemProps> = ({ event }) => {
 	return (
 		<Layout>
-			<h1>EVENT ITEM</h1>
+			<h1 className="fw-bold text-center mt-3">{event.slug}</h1>
 		</Layout>
 	)
 }
 
-// export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
-// 	console.log(params)
-// 	return {
-// 		props: {
-// 			test: ''
-// 		}
-// 	}
-// }
+export const getServerSideProps: GetServerSideProps = async ({ query }: GetServerSidePropsContext) => {
+	const { data } = await axios.get(`${API_URL}/api/events/${query.slug}`)
+	return {
+		props: {
+			event: data.events
+		}
+	}
+}
+
+interface IEventItemProps {
+	event: IEvent
+}
 
 export default EvenItem
