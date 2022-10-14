44664c66e5f63a8baab8bb0548610e39e4b5d95b
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-14 12:27:52 +0500

    path aliases added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import React, { PropsWithChildren } from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import styles from '../styles/Layout.module.css'
+import styles from '@styles/Layout.module.css'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 interface ILayout {
 	title?: string
