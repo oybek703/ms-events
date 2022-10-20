dca15343b3bf2452ff3f546dc259193cabbd184e
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-20 11:10:06 +0500

    custom route and controller added to strapi api

--- backend/src/api/event/controllers/event.ts
+++ backend/src/api/event/controllers/event.ts
@@ -4,8 +4,16 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::event.event', ({strapi}) => ({
-  async my() {
-    return {message: 'it is from core routes controller!'}
+export default factories.createCoreController('api::event.event', ({ strapi }) => ({
+  async my(ctx) {
+    const user = ctx.state.user
+    if (!user) return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was provided!' }] }])
+    const data = await strapi.entityService.findMany('api::event.event', {
+      filters: { user: user.id },
+      populate: {user: true}
+    })
+    if (!data || !data.length) return ctx.notFound()
+    const sanitizedEntity = await this.sanitizeOutput(data, ctx)
+    return this.transformResponse(sanitizedEntity, {})
   }
-}));
+}))
